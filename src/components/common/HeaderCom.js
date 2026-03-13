575ef6096c538c72d6b327128fe0890ef7dd7fbb
yhkim <xppzl@naver.com>
2026-03-13 12:26:12 +0900

    d

--- src/components/common/HeaderCom.js
+++ src/components/common/HeaderCom.js
@@ -51,7 +51,7 @@ const HeaderCom = () => {
                     <ul className="menu">
                         <li><Link to="/">사료</Link></li>
                         <li><Link to="/">간식</Link></li>
-                        <li><Link to="/list">목록보기</Link></li>
+                        <li><Link to="/list">목록보기!!</Link></li>
                         <li><Link to="/post/list">게시판</Link></li>
                         
                         {role === 'ROLE_ADMIN' && <>
